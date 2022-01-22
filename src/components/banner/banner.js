2b4bbb39435de3e42c1cc69f51501cc6a8584560
Colin <colin.d.chung@gmail.com>
2022-01-21 21:45:25 -0500

    banner ui

---
+++ src/components/banner/banner.js
@@ -0,0 +1,12 @@
+import './banner.css';
+
+function Banner() {
+  return (
+    <div className="banner">
+        <div className="banner-title">Hello, I'm <span className="text-highlight">Colin Chung.</span></div>
+        <div className="banner-subtitle">I'm a full-stack software developer.</div>
+    </div>
+  );
+}
+
+export default Banner;
