6cda3d8f90cd9103d206b6c81f23c09b02bb7b83
Colin <colin.d.chung@gmail.com>
2022-01-27 00:52:34 -0500

    convert to tailwind

---
+++ src/assets/images.js
@@ -0,0 +1,42 @@
+import headshot from "./about/headshot.jpg"
+import bk1 from "./backgrounds/bk1.png"
+import bk2 from "./backgrounds/bk2.jpg"
+import bk3 from "./backgrounds/bk3.jpg"
+import bk4 from "./backgrounds/bk4.jpg"
+
+import abp from "./logos/abp.png"
+import angular from "./logos/angular.png"
+import aws from "./logos/aws.png"
+import azure from "./logos/azure.png"
+import c from "./logos/c.png"
+import cpp from "./logos/cpp.png"
+import csharp from "./logos/csharp.png"
+import css3 from "./logos/css3.png"
+import django from "./logos/django.png"
+import docker from "./logos/docker.png"
+import dotnetCore from "./logos/dotnet-core.png"
+
+
+
+export default {
+    headshot,
+    backgrounds: {
+        bk1,
+        bk2,
+        bk3,
+        bk4,
+    },
+    logos: {
+        abp,
+        angular,
+        aws,
+        azure,
+        c,
+        cpp,
+        csharp,
+        css3,
+        django,
+        docker,
+        dotnetCore
+    }
+}
--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,8 +1,8 @@
 function Banner() {
   return (
-    <div className="banner">
-        <div className="banner-title">Hello, I'm <span className="text-highlight">Colin Chung.</span></div>
-        <div className="banner-subtitle">I'm a full-stack software developer.</div>
+    <div className="banner h-screen w-screen bg-repeat bg-cover bg-center bg-blend-darken flex flex-col items-center justify-start pt-10 sm:pt-0 sm:items-end sm:pr-16 sm:justify-center">
+        <div className="banner-font text-stone-100 text-2xl sm:text-4xl">Hello, I'm <span className="text-teal-500">Colin Chung.</span></div>
+        <div className="banner-font text-stone-100 text-1xl sm:text-2xl">I'm a full-stack software developer.</div>
     </div>
   );
 }
