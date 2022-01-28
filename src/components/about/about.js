2928040bec243186d6869dc24a41aac7a4ccb64f
Colin <colin.d.chung@gmail.com>
2022-01-27 23:34:41 -0500

    cleanup scrollbar

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -3,9 +3,9 @@ import images from '../../assets/images';
 function About() {
     return (
         <div className="component-container flex flex-col py-36 items-center">
-            <div className="component-title text-4xl">ABOUT</div>
+            <div className="font-poppins text-4xl">ABOUT</div>
             <div className="rounded flex flex-col md:flex-row justify-evenly items-center pt-10 px-10">
-                <img className="w-full headshot max-w-xs" src={images.headshot} alt="LinkedIn Headshot"/>
+                <img className="animate-wipe w-full headshot max-w-xs" src={images.headshot} alt="LinkedIn Headshot"/>
                 <div className="md:ml-20 py-4 max-w-lg">
                     <p className="text-gray-700 text-lg text-justify md:indent-16" style={{ fontFamily: "'Poppins', sans-serif"}}>
                         I'm a Computer Engineering student at The University of Waterloo and a Software Engineer at Wish. I am seeking SWE internship opportunities for Fall 2022 and new graduate opportunities for May 2023. I have skills and a passion for full-stack development, database design, application security and web development. In my personal time, you might find me cycling, playing basketball, playing tennis, watching movies or listening to hip hop music.
