ff84c31ef1c50cc60ecaa9508a7a2552ccbeaaae
Colin <colin.d.chung@gmail.com>
2022-02-02 01:02:07 -0500

    card data bdo

--- src/assets/card-data/bdo.js
+++ src/assets/card-data/bdo.js
@@ -4,10 +4,11 @@ const data = {
     logo: "bdo",
     date: "May 2020 - Aug 2020, Jan 2021 - Apr 2021",
     location: "Mississauga, Ontario",
+    description: "I joined BDO Canada as a Full-Stack Microsoft Developer for my 3rd and 4th Co-op placements. Both terms were remote due to COVID 19, but I got to work on several projects ranging from internal tools and client facing applications. Some highlights of my work at BDO include the database and backend design for a client facing application, creating an email invoice automation tool and implementing full-stack features for several applications.",
     bullets: [
-        "Developed full-stack features for an internal resource management application including profitability modelling, integration with third party services, multi-tenancy configuration and audit trails using .NET, Angular 7 and SQL.",
-        "Designed database schemas, developed API and proxy services using the ABP Framework, implemented UI branding and configured multi-tenancy and a role-based security framework for a data-entry web application.",
-        "Designed and developed an automated email service used for sending monthly invoices to all clients, reducing the duration of the process from several days to under 20 minutes."
+        "In my first term at BDO, I worked on an internal tool used for resource management with 2 other coop students and 1 full time developer. I was responsible for application support including many bug fixes and several new features. I implemented a profitability calculator where I was responsible for creating the UI, data models and API endpoints. I also implemented an audit trail feature that tracks all actions that affect the database and I created a corresponding record-based security framework.",
+        "Towards the end of my first internship, I was asked to also build an email automation tool that employees would use to send invoices to all clients. Since we had several hundred clients, manually sending out these emails would take an entire weekend. I designed a basic UI that allows the user to select a template, verify the email list and select an email signature. Using this tool, we were able to decrease the invoice email process to under 20 minutes total.",
+        "In my second internship, I was given the opportunity to take more ownership of a project when I joined a data mining project as the primary developer. I was given the freedom to design the database schema, build the backend data models and create the API endpoints. I was also in charge of configuring multitenancy for commercial use and a role-based security framework."
     ],
     stack: [
         "csharp",
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -21,16 +21,16 @@ function CardWork(props) {
                     <img className="max-w-2xs pt-5" alt="company logo" src={images.companies[props.data.logo]}/>
                 </div>
                 <div className="flex flex-col justify-between pr-2 pl-5 md:pl-10 md:pt-6 pb-2 lg:w-2/3">
-                    <div className="text-sm md:text-base text-justify mr-5 md:mr-8 indent-8">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    </div>
+                    <div className="text-sm md:text-base text-justify mr-5 md:mr-8 indent-8">{props.data?.description}</div>
                     <Collapse in={detailsOpen}>
-                        <div className='pt-2 text-sm md:text-base text-justify mr-5 md:mr-8 indent-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit, nisl at gravida suscipit, arcu orci dictum orci, eget bibendum ipsum urna et mauris. Quisque sit amet ornare nibh, ut rutrum ante. Aliquam porttitor tellus nec feugiat viverra. Quisque condimentum condimentum quam, ac cursus enim ornare sed. Vestibulum sed est in orci venenatis pharetra imperdiet porttitor sapien. Sed elementum nec risus tempor accumsan. Integer gravida lorem vel maximus scelerisque. Duis pellentesque malesuada venenatis.</div>
-                        <div className='pt-2 text-sm md:text-base text-justify mr-5 md:mr-8 indent-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit, nisl at gravida suscipit, arcu orci dictum orci, eget bibendum ipsum urna et mauris. Quisque sit amet ornare nibh, ut rutrum ante. Aliquam porttitor tellus nec feugiat viverra. Quisque condimentum condimentum quam, ac cursus enim ornare sed. Vestibulum sed est in orci venenatis pharetra imperdiet porttitor sapien. Sed elementum nec risus tempor accumsan. Integer gravida lorem vel maximus scelerisque. Duis pellentesque malesuada venenatis.</div>
-                        <div className='pt-2 text-sm md:text-base text-justify mr-5 md:mr-8 indent-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit, nisl at gravida suscipit, arcu orci dictum orci, eget bibendum ipsum urna et mauris. Quisque sit amet ornare nibh, ut rutrum ante. Aliquam porttitor tellus nec feugiat viverra. Quisque condimentum condimentum quam, ac cursus enim ornare sed. Vestibulum sed est in orci venenatis pharetra imperdiet porttitor sapien. Sed elementum nec risus tempor accumsan. Integer gravida lorem vel maximus scelerisque. Duis pellentesque malesuada venenatis.</div>
+                        {
+                            props.data?.bullets.map(b => (
+                                <div className='pt-2 text-sm md:text-base text-justify mr-5 md:mr-8 indent-8' key={b.slice(10)}>{b}</div>
+                            ))
+                        }
                     </Collapse>
                     <button 
-                        className="self-end bg-slate-300 hover:bg-gray-400 text-gray-800 font-bold mb-2 py-2 px-4 rounded inline-flex items-center"
+                        className="self-end bg-slate-300 hover:bg-gray-400 text-gray-800 font-bold mt-2 mb-2 py-2 px-4 rounded inline-flex items-center"
                         onClick={toggleDetails}
                     >
                         <img className="icon-xs pr-2" src={ detailsOpen ? images.svg.upArrow : images.svg.downArrow} alt="down-arrow"/>
