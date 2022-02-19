abde05d6b3a569794058447fb57d592b6242e105
Colin <colin.d.chung@gmail.com>
2022-02-19 00:08:47 -0500

    update contact

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -2,7 +2,7 @@ import images from '../../assets/images'
 
 function Icon(props) {
     return (
-        <div className="p-3 mx-3 hover:bg-teal-500 rounded-sm">
+        <div className="p-2 mx-3 hover:bg-teal-500 rounded-sm">
             <a href={props.href} target="_blank" rel="noopener noreferrer">
                 <img className="icon-sm" src={images.contact[props.img]}/>
             </a>
@@ -17,7 +17,6 @@ function Contact() {
                 <Icon href="https://www.linkedin.com/in/colindchung/" img="linkedIn"/>
                 <Icon href="https://github.com/colindchung" img="github"/>
                 <Icon href="https://devpost.com/colindchung" img="devpost"/>
-                <Icon href="https://www.instagram.com/colindchung/" img="instagram"/>
             </div>
         </div>
     )
