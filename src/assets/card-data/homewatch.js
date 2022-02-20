73788e9241f7a0a7a1f936f03583ff59f546f562
Colin <colin.d.chung@gmail.com>
2022-02-19 19:52:34 -0500

    homewatch data

--- src/assets/card-data/homewatch.js
+++ src/assets/card-data/homewatch.js
@@ -5,9 +5,16 @@ const data = {
     location: "Waterloo, Ontario",
     github: "https://github.com/colindchung/HomeWatch",
     devpost: "https://devpost.com/software/homewatch",
+    description: "HomeWatch is a product I made with three other peers at Hack the North 2019. It is an AI powered home security system that performs facial and license plate recognition to provide more insights than a traditional system. It monitors the door and driveway and performs facial recognition on anyone approaching and parses the license plate of any vehicles that enter the driveway. With this functionality in conjunction with the web and mobile applications we created, homeowners can have full transparency about who is approaching their home.",
     bullets: [
-        "Created a home monitoring and security system using cameras to notify users of unknown vehicles and people approaching their home.",
-        "Developed a cross-platform mobile application using React Native and Firebase which provides updates on home status in real-time."
+        {
+            type: "text",
+            value: "I was in charge of designing and developing the mobile application for the product. The mobile app is meant to allow users to log in and see a live history of all people and cars that have recently entered the driveway. I designed and created the login and registration screens and used Firebase Authentication to allow users to log in and sign up for the service."
+        },
+        {
+            type: "text",
+            value: "Once the user is logged in, they need to see live data from our Cloud Firestore database. I was in charge of managing the response objects from our Tesseract/OpenCV endpoints and storing them in our database. Then, using Firebase’s Real-Time Database, we were able to consume live updates without refreshing the page."
+        }
     ],
     stack: [
         "react",
@@ -18,7 +25,18 @@ const data = {
         "opencv",
         "tesseract"
     ],
-    links: []
+    links: [
+        {
+            label: "GitHub",
+            url: "https://github.com/colindchung/HomeWatch",
+            seq: 1
+        },
+        {
+            label: "Devpost",
+            url: "https://devpost.com/software/homewatch",
+            seq: 2
+        }
+    ]
 }
 
 export default data
