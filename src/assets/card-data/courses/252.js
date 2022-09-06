bb830c030489725af6a38ce76f9d0e6d637cba54
Colin <colin.d.chung@gmail.com>
2022-09-06 17:56:49 -0400

    252 data

---
+++ src/assets/card-data/courses/252.js
@@ -0,0 +1,22 @@
+const data = {
+    code: "ECE252",
+    title: "ECE252: Systems Programming and Concurrency",
+    company: "University of Waterloo",
+    logo: "waterloo",
+    date: "Sep 2020 - Dec 2020",
+    grade: "88",
+    description: "Processes and threads (pthreads); system calls; concurrency (semaphore, mutex, monitors, and barrier synchronization); user-level memory management. Performance and correctness of concurrent systems. Deadlock detection and recovery; file systems.",
+    topics: "The major topics in this course were process and thread management, system calls, concurrency (mutexes, semaphores, monitors and barrier synchronization) and user-level memory management. The labs used web crawling and image processing as busy work to focus on concurrency and performance optimization using inter-process communication, non-blocking I/O and the producer/consumer model",
+    stack: [
+        {
+            tooltip: "C",
+            img: "c"
+        },
+        {
+            tooltip: "Linux",
+            img: "linux"
+        },
+    ]
+}
+
+export default data
--- src/components/courses/courses.js
+++ src/components/courses/courses.js
@@ -2,12 +2,14 @@ import CardCourses from '../card/card-courses'
 import ece124Data from '../../assets/card-data/courses/124'
 import ece150Data from '../../assets/card-data/courses/150'
 import ece250Data from '../../assets/card-data/courses/250'
+import ece252Data from '../../assets/card-data/courses/252'
 
 function Courses() {
     return (
         <div className="component-container flex flex-col pt-48 pb-64 items-center bg-slate-700 min-h-screen top-slant">
             <div className="font-poppins text-4xl text-teal-500 mb-10">COURSE WORK</div>
 
+            <CardCourses data={ece252Data}/>
             <CardCourses data={ece250Data}/>
             <CardCourses data={ece124Data}/>
             <CardCourses data={ece150Data}/>
