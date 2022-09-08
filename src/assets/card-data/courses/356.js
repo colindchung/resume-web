8740dd8d54092b92bcf4dce8b18aff2426223ee2
Colin <colin.d.chung@gmail.com>
2022-09-08 16:15:44 -0400

    356, 358 data

---
+++ src/assets/card-data/courses/356.js
@@ -0,0 +1,22 @@
+const data = {
+    code: "ECE356",
+    title: "ECE356: Database Systems",
+    company: "University of Waterloo",
+    logo: "waterloo",
+    date: "Sep 2021 - Dec 2021",
+    grade: "70",
+    description: "Data models, file systems, database system architectures, query languages, integrity and security, database design.",
+    topics: "The course covered relational algebra, the entity relationship model, database normalization and optimization with indexing and functional dependency management. The final project involved taking a large dataset seed and creating a user interface that could process and query this data in a useful way.",
+    stack: [
+        {
+            tooltip: "MySQL",
+            img: "mysql"
+        },
+        {
+            tooltip: "Python",
+            img: "python"
+        }
+    ]
+}
+
+export default data
--- src/components/courses/courses.js
+++ src/components/courses/courses.js
@@ -6,12 +6,16 @@ import ece252Data from '../../assets/card-data/courses/252'
 import ece327Data from '../../assets/card-data/courses/327'
 import ece350Data from '../../assets/card-data/courses/350'
 import ece320Data from '../../assets/card-data/courses/320'
+import ece356Data from '../../assets/card-data/courses/356'
+import ece358Data from '../../assets/card-data/courses/358'
 
 function Courses() {
     return (
         <div className="component-container flex flex-col pt-48 pb-64 items-center bg-slate-700 min-h-screen top-slant">
             <div className="font-poppins text-4xl text-teal-500 mb-10">COURSE WORK</div>
 
+            <CardCourses data={ece358Data}/>
+            <CardCourses data={ece356Data}/>
             <CardCourses data={ece320Data}/>
             <CardCourses data={ece350Data}/>
             <CardCourses data={ece327Data}/>
